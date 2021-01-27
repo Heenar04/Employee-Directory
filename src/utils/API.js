76c78a63e74cbf2d556ee30b4b6c0b6dea01f2fa
hrajpal <hrajpal@ebrevia.com>
2021-01-27 13:55:25 -0500

    worked on reportwebvitals

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     getUser: function(){
-        return axios.get('https://randomuser.me/api/?BFIT-2AWX-BDKB-U6WB');
+        return axios.get('https://randomuser.me/api/?results=150&nat=us');
     }
 }
